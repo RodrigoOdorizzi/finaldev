79a74b7a1d834fa978660b854c343e31a6e4e993
rodrigo <rodrigoodorizzi@hotmail.com>
2021-07-20 21:09:08 -0300

    teste

---
+++ index2 - Copia.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <title>Calculando P.A.</title>
+</head>
+
+<body>
+    <form action="" method="post"><br />
+        <label>Entre com o Número de Termos da P.A: </label>
+        <input type="text" name="termos" size="3" /><br />
+        <label>Entre com o Primeiro Termos da P.A: </label>
+        <input type="text" name="primeiro" size="3"><br />
+        <label>Entre com a Razão da P.A: </label>
+        <input type="text" name="razao" size="3"><br />
+        <input type="submit" name="Enviar" value="enviar"><br /><br />
+    </form>
+    <?php
+    function pa($n, $p, $r)
+    {
+
+        $soma = $p;
+        echo $soma;
+        for ($i = 1; $i <= $n - 1; $i++) {
+            $soma = ($soma + $r);
+            echo "," . "$soma";
+        }
+        echo "<br/>";
+    }
+    $n = $_POST['termos'];
+    $p = $_POST['primeiro'];
+    $r = $_POST['razao'];
+    pa($n, $p, $r);
+    ?>
+</body>
+
+</html>
